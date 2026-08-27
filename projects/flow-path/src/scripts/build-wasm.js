11fd978ec5def468c66f82276457c7fc6d59519d
Christoph Wieske <christoph.wieske@wieske.io>
2026-08-27 13:19:34 +0200

    Fixes CI related script.

--- projects/flow-path/src/scripts/build-wasm.js
+++ projects/flow-path/src/scripts/build-wasm.js
@@ -1,6 +1,13 @@
-// scripts/embed-wasm.js
+const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
+
+const projectRoot = path.resolve(__dirname, '..');
+
+execSync('wasm-pack build lib/flow-path/path-finders/a-star-wasm --target web --release', {
+  cwd: projectRoot,
+  stdio: 'inherit',
+});
 
 const input = path.resolve(
   __dirname,
@@ -24,3 +31,13 @@ export default wasmBytes;
 
 fs.writeFileSync(output, content, 'utf8');
 console.log(`Embedded WASM written to ${output}`);
+
+const dtsFile = path.resolve(
+  __dirname,
+  '../lib/flow-path/path-finders/a-star-wasm/pkg/a_star_rust.d.ts',
+);
+
+if (fs.existsSync(dtsFile)) {
+  fs.unlinkSync(dtsFile);
+  console.log(`Deleted ${dtsFile}`);
+}
